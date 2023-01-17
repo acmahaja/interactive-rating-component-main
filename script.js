df1e1a7b335cf6d1e78a8e972e942b1f7bab2622
acmahaja <acmahaja@gmail.com>
2023-01-17 18:27:08 +0200

    Added interactivity to button

--- script.js
+++ script.js
@@ -1,7 +1,20 @@
-const form = document.querySelector('form');
-form.onsubmit = submitted.bind(form);
+let rating = -1;
+let submitted = false;
 
-function submitted(event) {
-    event.preventDefault()
+function submit(event) {
     console.log("Submitting");
+}
+
+
+function selectedRating(number){
+    rating = number;
+    let buttons = document.getElementsByClassName("button")
+    for (let i = 0; i < buttons.length; i++) {
+        try {
+            buttons[i].classList.remove('selected');
+        } catch (error) {
+            continue;
+        }
+    }
+    document.getElementById(number).classList.add('selected')
 }
